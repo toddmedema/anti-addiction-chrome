264d495e4b7cce2c7c3fbde8662abca1a82e027f
Todd Medema <toddmedema@gmail.com>
2020-01-08 08:46:13 -0500

    1.1.1

--- js/options.js
+++ js/options.js
@@ -7,7 +7,7 @@ function restore_options() {
     message: 'Shouldn\'t you be doing something else?',
     greyscale: 100,
     urls: ['facebook.com', 'reddit.com', 'imgur.com', 'instagram.com', 'mail.google.com', 'pinterest.com', 'amazon.com', 'feedly.com', 'buzzfeed.com', 'thisiswhyimbroke.com', 'iwastesomuchtime.com', 'pinterest.com', 'failblog.org', 'overstock.com', 'popcap.com'],
-    version: '1.1.0',
+    version: '1.1.1',
     // DEFAULTS - also change in content.js
   }, (items) => {
     document.getElementById('delay').value = Math.round(items.delay / 1000);
