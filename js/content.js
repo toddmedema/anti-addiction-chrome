b99b27793e0a41dfe11c23c45efb82cf3e9c2fdc
Todd Medema <toddmedema@gmail.com>
2020-01-20 11:07:27 -0500

    1.2 - nuclear greyscale option

--- js/content.js
+++ js/content.js
@@ -1,44 +1,44 @@
 chrome.storage.sync.get({
   delay: 2000,
   greyscale: 100,
+  nuclear: false,
   message: 'Shouldn\'t you be doing something else?',
   urls: ['facebook.com', 'reddit.com', 'imgur.com', 'instagram.com', 'mail.google.com', 'pinterest.com', 'amazon.com', 'feedly.com', 'buzzfeed.com', 'thisiswhyimbroke.com', 'iwastesomuchtime.com', 'pinterest.com', 'failblog.org', 'overstock.com', 'popcap.com'],
   // DEFAULTS - also change in options.js
-}, (settings) => {
-  if (settings.urls.some((check) => window.location.href.indexOf(check) >= 0)) {
-    delaySite(settings);
+}, init);
+
+function init(settings) {
+  var siteIsActive = settings.urls.some((check) => window.location.href.indexOf(check) >= 0);
+  if (siteIsActive) {
+    var div = document.createElement('div');
+    div.id = "antiaddiction";
+    div.innerHTML = 'Shouldn\'t you be doing something else?';
+    div.style.cssText = `
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: 100;
+      background: #FFF;
+      color: #000;
+      padding: 5%;
+      font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+      font-size: 2em;
+      text-align: center;
+      z-index: 2147483647;
+    `;
+    document.body.appendChild(div);
+
+    setTimeout(() => {
+      var elem = document.querySelector('#antiaddiction');
+      if (elem) {
+        elem.parentNode.removeChild(elem);
+      }
+    }, settings.delay);
   }
-});
 
-function delaySite(settings) {
-  var div = document.createElement('div');
-  div.id = "antiaddiction";
-  div.innerHTML = 'Shouldn\'t you be doing something else?';
-  div.style.cssText = `
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 100;
-    background: #FFF;
-    color: #000;
-    padding: 5%;
-    font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
-    font-size: 2em;
-    text-align: center;
-    z-index: 2147483647;
-  `;
-  document.body.appendChild(div);
-
-  if (settings.greyscale) {
+  if (settings.greyscale && (siteIsActive || settings.nuclear)) {
     document.body.style.cssText = document.body.style.cssText + ` filter: grayscale(${settings.greyscale}%);`;
   }
-
-  setTimeout(() => {
-    var elem = document.querySelector('#antiaddiction');
-    if (elem) {
-      elem.parentNode.removeChild(elem);
-    }
-  }, settings.delay);
 }
--- js/options.js
+++ js/options.js
@@ -6,12 +6,14 @@ function restore_options() {
     delay: 10000,
     message: 'Shouldn\'t you be doing something else?',
     greyscale: 100,
+    nuclear: false,
     urls: ['facebook.com', 'reddit.com', 'imgur.com', 'instagram.com', 'mail.google.com', 'pinterest.com', 'amazon.com', 'feedly.com', 'buzzfeed.com', 'thisiswhyimbroke.com', 'iwastesomuchtime.com', 'pinterest.com', 'failblog.org', 'overstock.com', 'popcap.com'],
-    version: '1.1.1',
+    version: '1.2.0',
     // DEFAULTS - also change in content.js
   }, (items) => {
     document.getElementById('delay').value = Math.round(items.delay / 1000);
     document.getElementById('message').value = items.message;
+    document.getElementById('nuclear').checked = items.nuclear;
     document.getElementById('greyscale').value = items.greyscale;
     document.getElementById('urls').value = items.urls.join(',');
   });
@@ -26,6 +28,7 @@ function save_options() {
   chrome.storage.sync.set({
     delay: document.getElementById('delay').value * 1000,
     message: document.getElementById('message').value,
+    nuclear: document.getElementById('nuclear').checked,
     greyscale: Math.min(100, Math.max(0 ,+document.getElementById('greyscale').value)),
     urls: document.getElementById('urls').value.split(',').map((url) => url.trim()),
   }, () => {
